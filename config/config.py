3486ecae957eed3fcf565c45c4f2435e9292a9a3
canhdinhtien <131688279+canhdinhtien@users.noreply.github.com>
2026-03-03 22:31:35 +0700

    chrore: update config

--- config/config.py
+++ config/config.py
@@ -3,36 +3,50 @@ import ml_collections
 def get_config():
     config = ml_collections.ConfigDict()
 
-    config.run_name = ""
+    config.run_name = ""                
     config.seed = 42
     config.logdir = "logs"
-
-    config.num_epochs = 100
-    config.save_freq = 20
-
-    config.num_checkpoint_limit = 5
-    config.mixed_precision = "fp16"
-
+    config.mixed_precision = "fp16"     # "no", "fp16", "bf16"
     config.allow_tf32 = True
-    config.resume_from = ""
+    config.resume_from = ""             
 
+    config.dataset_name = "dtd"         
+    config.n_shot = 16                  
+    config.is_synth_train = False       
+    config.is_pooled_fewshot = False    
+    config.model_type = "clip"
     config.use_lora = True
-    config.n_shot = 16
 
     config.pretrained = pretrained = ml_collections.ConfigDict()
     pretrained.model = "sd2-community/stable-diffusion-2-1"
+    pretrained.revision = "main"
+    pretrained.use_tiny_decoder = False
+
+    config.classifier = classifier = ml_collections.ConfigDict()
+    classifier.is_lora_image = True
+    classifier.is_lora_text = False
+    classifier.clip_download_dir = "model_clip"
+    classifier.clip_version = "ViT-B/16"
+    classifier.precomputed_text_embs_path = "weights\dtd_text_emb.pt"
 
     config.train = train = ml_collections.ConfigDict()
     train.batch_size = 32
+    train.num_epochs = 100
+    train.save_freq = 20
+    train.num_checkpoint_limit = 5     
     train.use_8bit_adam = False
-    train.lr = 3e-4
-    
+    train.lr = 1e-4
+    train.is_rand_aug = True
+    train.adam_beta1 = 0.9
+    train.adam_beta2 = 0.999      
+    train.adam_weight_decay = 1e-2
+    train.adam_epsilon = 1e-10 # Default is 1e-8
+
     config.path = path = ml_collections.ConfigDict()
-    path.real_train_dir = "real_data"
-    path.fewshot_dir = "real_data/train/dtd/fewshot_real_images"
+    path.real_train_dir = "real_data"   
+    path.real_test_dir = "real_data"
+    path.metadata_dir = "metadata"      
+    path.fewshot_dir = "real_data/train/dtd/fewshot_real_images" 
     path.synthesis_dir = "synthetic_data"
 
-
     return config
-
-    
