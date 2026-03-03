5540ff25f69274abeeee6cf9031847d23dddc212
canhdinhtien <canhva20047@gmail.com>
2026-03-03 16:37:48 +0700

    update: add main

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+from polars import datetime
+import tqdm
+import torch
+
+from collections import defaultdict
+from functools import partial
+
+from ml_collections import config_flags
+from flask import config
+from absl import app, flags
+
+from accelerate import Accelerator
+from accelerate.utils import set_seed, ProjectConfiguration
+from accelerate.logging import get_logger
+
+tqdm = partial(tqdm.tqdm, dynamic_ncols=True) # fix tqdm not showing progress bar
+
+FLAGS = flags.FLAGS
+config_flags.DEFINE_config_file("config", "config/base.py", "Training configuration.")
+
+def main():
+    config = FLAGS.config
+    unique_id = datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
+    if not config.run_name:
+        config.run_name = unique_id
+    else:
+        config.run_name += "_" + unique_id
+
+    if config.resume_from:
+        config.resume_from = os.path.normpath(os.path.expanduser(config.resume_from))
+        if "checkpoint_" not in os.path.basename(config.resume_from):
+            # get the most recent checkpoint in this directory
+            checkpoints = list(
+                filter(lambda x: "checkpoint_" in x, os.listdir(config.resume_from))
+            )
+            if len(checkpoints) == 0:
+                raise ValueError(f"No checkpoints found in {config.resume_from}")
+            config.resume_from = os.path.join(
+                config.resume_from,
+                sorted(checkpoints, key=lambda x: int(x.split("_")[-1]))[-1],
+            )
+
+    accelerator_config = ProjectConfiguration(
+        project_dir=os.path.join(config.logdir, config.run_name),
+        automatic_checkpoint_naming=True,
+        total_limit=config.num_checkpoint_limit,
+    )
+
+    accelerator = Accelerator(
+        log_with="wandb",
+        mixed_precision=config.mixed_precision,
+        project_config=accelerator_config,
+    )
