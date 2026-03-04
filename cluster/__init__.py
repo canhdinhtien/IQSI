1142be679ce26814d6cb85e0b2687a3d2e4509a5
canhdinhtien <canhva20047@gmail.com>
2026-03-04 17:37:31 +0700

    feat: implement centroid computing in cluster package

---
+++ cluster/__init__.py
@@ -0,0 +1,1 @@
+from .cluster import get_centroids_from_loader, assign_to_regions
---
+++ cluster/cluster.py
@@ -0,0 +1,39 @@
+import torch
+import faiss
+import numpy as np
+from PIL import Image
+
+def get_centroids_from_loader(model, paths, n_clusters, clean_transform, batch_size, accelerator, dtype_clip):
+    all_embeddings = []
+    
+    for i in range(0, len(paths), batch_size):
+        chunk_paths = paths[i : i + batch_size]
+        batch_tensors = []
+        
+        for p in chunk_paths:
+            try:
+                img = Image.open(p).convert('RGB')
+                batch_tensors.append(clean_transform(img))
+            except Exception as e:
+                print(f"Lỗi khi nạp ảnh {p}: {e}")
+                continue
+        
+        if not batch_tensors:
+            continue
+
+        batch_input = torch.stack(batch_tensors).to(accelerator.device, dtype=dtype_clip)
+
+        image_feats = model.forward_image(batch_input)
+        all_embeddings.append(image_feats.cpu())
+
+    all_embeddings = torch.cat(all_embeddings, dim=0).float().numpy()
+    
+    d_model = all_embeddings.shape[1]
+    kmeans = faiss.Kmeans(d_model, n_clusters, niter=20, verbose=True, gpu=True)
+    kmeans.train(all_embeddings)
+
+    return kmeans.centroids
+
+def assign_to_regions(features, centroids):
+    distances = torch.cdist(features.float(), centroids.float(), p=2)
+    return torch.argmin(distances, dim=1)
